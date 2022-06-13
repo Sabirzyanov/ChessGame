111ff84b6fcfc94fe66d3e11a38def38e2570a21
Sabirzyanov <sabirzyanov439@yandex.ru>
2022-06-13 22:09:39 +0300

    Created Main Gmae board and other main logic

--- Chess.Core/PieceMaker.cs
+++ Chess.Core/PieceMaker.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chess.Core
 {
@@ -18,7 +14,7 @@ namespace Chess.Core
                 "Knight" => new Knight(x, y),
                 "Queen" => new Queen(x, y),
                 "Rook" => new Rook(x, y),
-                _ => new Pawn(x, y),
+                _ => new Pawn(x, y)
             };
         }
 
