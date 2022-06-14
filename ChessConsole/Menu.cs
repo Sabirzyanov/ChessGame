5c45b8a557ec3f314fec63e00b3fb9a597546327
Sabirzyanov <sabirzyanov439@yandex.ru>
2022-06-14 22:42:59 +0300

    Add Menu to Console Project

---
+++ ChessConsole/Menu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ChessConsole
+{
+    public class Menu
+    {
+        private int SelectOption;
+        private string Prompt;
+        private string[] Options;
+
+
+        public Menu(string[] options, string prompt)
+        {
+            Options = options;
+            Prompt = prompt;
+            SelectOption = 0;
+        }
+
+        public Menu(string[] options)
+        {
+            Options = options;
+            SelectOption = 0;
+        }
+
+        private void DisplayOption()
+        {
+            Console.WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string prefix;
+                if (i == SelectOption)
+                {
+                    prefix = "*";
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                }
+                
+                Console.WriteLine($" {prefix} <--- {Options[i]} --- >");
+            }
+            Console.ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey pressedKey;
+            do
+            {
+                Console.Clear();
+                DisplayOption();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                pressedKey = keyInfo.Key;
+
+                if (pressedKey == ConsoleKey.UpArrow)
+                {
+                    SelectOption--;
+                    if (SelectOption == -1)
+                    {
+                        SelectOption = Options.Length - 1;
+                    }
+                } 
+                else if (pressedKey == ConsoleKey.DownArrow)
+                {
+                    SelectOption++;
+                    if (SelectOption == Options.Length)
+                    {
+                        SelectOption = 0;
+                    }
+                }
+            } while(pressedKey != ConsoleKey.Enter);
+
+            return SelectOption;
+        }
+    }
+}
--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,13 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 using Chess.Core;
 
 namespace ChessConsole
 {
     internal class Program
     {
+        static List<Piece> pieces = new List<Piece>();
+        static string _selectedFigureType = "";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            StartMenu();
+        }
+
+        public static void StartMenu()
+        {
+            string[] options =
+            {
+                "Add figure", "Exit"
+            };
+            Menu menu = new Menu(options);
+            int selectedOption = menu.Run();
+
+            switch (selectedOption)
+            {
+                case 0:
+                    SelectFigure();
+                    break;
+                case 1:
+                    Exit();
+                    break;
+            }
+        }
+
+        private static void SelectFigure()
+        {
+            string[] figures =
+            {
+                "Bishop", "King", "Knight", "Pawn", "Queen", "Rook", "Exit"
+            };
+            Menu menu = new Menu(figures);
+            int selectedOption = menu.Run();
+            if (selectedOption == figures.Length - 1)
+            {
+                StartMenu();
+            }
+            _selectedFigureType = figures[selectedOption];
+
+        }
+
+        private void AddFigure()
+        {
+            Console.Write("Enter Coordinates of Piece as Chess - (A-H)(1-8):");
+            string coords = Console.ReadLine();
+
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+            Environment.Exit(0);
         }
     }
 }
